630420fa4e1cb9aa52e5c3f65ed2f4f4286cfd15
villimani <vilmundurmani@gmail.com>
2023-02-17 09:09:02 +0000

    Initial commit

---
+++ src/main/java/is/hi/vmt7/hbv202g/ass4/App.java
@@ -0,0 +1,13 @@
+package is.hi.vmt7.hbv202g.ass4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/is/hi/vmt7/hbv202g/ass4/AppTest.java
@@ -0,0 +1,20 @@
+package is.hi.vmt7.hbv202g.ass4;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
